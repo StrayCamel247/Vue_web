76134533c7ef2836f38f8544691d301a45097bf8
Freen <aboyinsky@outlook.com>
2020-10-13 16:20:08 +0800

    update 登陆路由接通

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://192.168.21.55:8000/',
+  baseURL: 'http://localhost:8000/',
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
